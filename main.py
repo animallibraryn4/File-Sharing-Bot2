221db39de59c7ab5fa6141ee17d7e23494033860
animallibraryn4 <nikhil7858978052@gmail.com>
2025-01-26 22:38:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from bot import Bot
+# In main.py or your bot start logic
+from config import initialize_force_sub_channels
 
+# Call it to populate FORCE_SUB_CHANNELS at the start
+await initialize_force_sub_channels()
 Bot().run()
